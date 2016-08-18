18de830b236eff71f9c2915c5b57d73a11e9dc27
Kaan Karaca <kaan94karaca@gmail.com>
2016-08-18 19:15:51 +0300

    Bootstrap-PHP

--- privacypolicy.php
+++ privacypolicy.php
@@ -11,13 +11,14 @@
 
 		<section class="container">
 			<div class="content row">
+        <?php include "_/components/php/header.php"; ?>
 				<section class="main col col-lg-8">
-
 				</section><!-- main -->
 				<section class="sidebar col col-lg-4">
 
 				</section><!-- sidebar -->
 			</div><!-- content -->
+    <?php include "_/components/php/footer.php"; ?>
 		</section><!-- container -->
 
     <script src="_/js/bootstrap.js"></script>
